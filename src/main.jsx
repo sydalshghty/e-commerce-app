686de69154b0383a4977be821681f342e1ed2e9d
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2025-06-11 13:46:22 +0200

    update-changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-//import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { HashRouter } from 'react-router-dom';
+//import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <BrowserRouter basename='e-commerce-app'>
         <Provider store={store}>
             <App />
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 )
